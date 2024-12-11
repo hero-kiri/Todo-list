eae4bd629e6cc391c1a6f79866778ec416d99414
kiri3914 <t.bektur.2002@gmail.com>
2024-12-11 12:17:13 +0500

    add comment

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-
+# Это пути 
 urlpatterns = [
     path('', views.index, name='index'),
     path('todo_list', views.todo_list, name='todo_list')
 ]
+
